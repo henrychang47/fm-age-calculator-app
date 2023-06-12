41f4648015ece4337e3b10cf39077c277863fafe
henrychang47 <henry881116@gmail.com>
2023-06-12 12:04:28 +0800

    checkInputs func

--- src/script.js
+++ src/script.js
@@ -1,26 +1,81 @@
+const form = document.querySelector('form');
 const dayInput = document.querySelector('#dayInput');
 const monthInput = document.querySelector('#monthInput');
 const yearInput = document.querySelector('#yearInput');
 const calBtn = document.querySelector('.calBtn');
+const yearNum = document.querySelector('#yearNum');
+const monthNum = document.querySelector('#monthNum');
+const dayNum = document.querySelector('#dayNum');
+const errorMsgs = document.querySelectorAll('.errorMsg');
 
 calBtn.addEventListener('click', executeCal);
 
-function getValue(type) {
-  switch (type) {
-    case 'day':
-      return dayInput.value;
-    case 'month':
-      return monthInput.value;
-    case 'year':
-      return yearInput.value;
-    default:
-      return undefined;
+function getValue() {
+  return {
+    'day': dayInput.value,
+    'month': monthInput.value,
+    'year': yearInput.value,
   }
 }
 
-function executeCal() {
-  console.log(getValue('day'));
+function setDate({ year, month, day }) {
+  yearNum.innerText = year;
+  monthNum.innerText = month;
+  dayNum.innerText = day;
 }
 
+function executeCal() {
+  checkInputs();
+}
 
+function checkInputs() {
+  let { day, month, year } = getValue();
+  clearInvalid();
 
+  if (day === '') {
+    displayErrorMsg('day', 'empty');
+  }
+  else {
+    day = Number(day);
+    if (isNaN(day) || day < 1 || day > 31) displayErrorMsg('day', 'invalid');
+  }
+  if (month === '') {
+    displayErrorMsg('month', 'empty')
+  } else {
+    month = Number(month);
+    if (isNaN(month) || month < 1 || month > 12) displayErrorMsg('month', 'invalid');
+  }
+  if (year === '') {
+    displayErrorMsg('year', 'empty');
+  } else {
+    year = Number(year);
+    if (isNaN(year) || year < 1) displayErrorMsg('year', 'invalid');
+  }
+}
+
+function displayInvalid() {
+  form.classList.add('invalid');
+}
+
+function clearInvalid() {
+  form.classList.remove('invalid');
+  errorMsgs.forEach(msg => msg.innerText = '');
+}
+
+function displayErrorMsg(target, errorType) {
+  displayInvalid();
+
+  const errorMsg = {
+    'empty': 'This field is required',
+    'invalid': `Must be a valid ${target}`
+  }
+
+  const targetIndex = {
+    'day': 0,
+    'month': 1,
+    'year': 2
+  }[target];
+
+  errorMsgs[targetIndex].classList.add('display');
+  errorMsgs[targetIndex].innerText = errorMsg[errorType];
+}
